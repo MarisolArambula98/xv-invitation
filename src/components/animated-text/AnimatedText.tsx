452fed9d82e247c3a63bd0861c71b37a2527a641
ericksluna053 <62397015+ericksluna053@users.noreply.github.com>
2025-05-21 23:16:19 -0600

    clean code 2

--- src/components/animated-text/AnimatedText.tsx
+++ src/components/animated-text/AnimatedText.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './AnimatedText.css';
 
 interface CursiveTextProps {
--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react';
 import './Gallery.css';
 
 const Gallery: React.FC = () => {
