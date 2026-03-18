5465d03137d16cfc744c9175443f2cc71500e96a
ericksluna053 <62397015+ericksluna053@users.noreply.github.com>
2026-03-17 20:53:25 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,15 @@ import './App.css'
 import Parchment from './components/parchment/Parchment'
 
 
+//comentario
+
 function App() {
 
   return (
     <>
+      <>
+        test
+      </>
       <div className="parchment__container">
         <Parchment/>
       </div>
